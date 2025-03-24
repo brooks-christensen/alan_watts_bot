24b88b40c75dade88162dafd1d4510e66bbb8119
Brooks Christensen <peacelovephysics@gmail.com>
2025-03-24 14:54:53 -0500

    refactored training

--- language_model/train/train.py
+++ language_model/train/train.py
@@ -1,3 +1,5 @@
+import sched
+import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import time
 from loguru import logger
@@ -7,9 +9,136 @@ from torch.utils.data import DataLoader
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import LambdaLR
 import os
-from torch import load as torchload
+from torch import load as torchload, device as torchDevice, Tensor
 
 from ..utils.utils import format_time
+
+
+def train_batch(
+    epoch_i: int, 
+    config: dict, 
+    model: GPT2LMHeadModel, 
+    train_dataloader: DataLoader, 
+    device: torchDevice,
+    input_ids: Tensor, 
+    attention_mask: Tensor, 
+    tokenizer: GPT2Tokenizer, 
+    optimizer: AdamW, 
+    scheduler: LambdaLR,
+    ):
+
+    logger.info("")
+    logger.info('======== Epoch {:} / {:} ========'.format(epoch_i + 1, config['training']['epochs']))
+    logger.info('Training...')
+
+    t0 = time.time()
+
+    total_train_loss = 0
+
+    model.train()
+
+    for step, batch in enumerate(train_dataloader):
+
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[0].to(device)
+        b_masks = batch[1].to(device)
+
+        model.zero_grad()
+
+        outputs = model(  
+            b_input_ids,
+            labels=b_labels,
+            attention_mask = b_masks,
+            token_type_ids=None
+        )
+
+        loss = outputs[0]
+
+        batch_loss = loss.item()
+        total_train_loss += batch_loss
+
+        # Get sample every x batches.
+        if step % config['training']['sample_every'] == 0 and not step == 0:
+
+            elapsed = format_time(time.time() - t0)
+            logger.info('  Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Total Elapsed this Epoch: {:}.'.format(step, len(train_dataloader), batch_loss, elapsed))
+
+            model.eval()
+
+            # sample_outputs = model.generate(
+            #     bos_token_id=random.randint(1,30000),
+            #     do_sample=True,
+            #     top_k=50,
+            #     max_length = 200,
+            #     top_p=0.95,
+            #     num_return_sequences=1
+            # )
+
+            with no_grad():
+                sample_outputs = model.generate(
+                    # generated,
+                    input_ids=input_ids,
+                    attention_mask=attention_mask,  # Include the attention mask
+                    # bos_token_id=random.randint(1,30000),
+                    do_sample=config['generation']['do_sample'],
+                    temperature=config['generation']['temperature'],
+                    top_k=config['generation']['top_k'],
+                    max_length=config['generation']['max_length'],
+                    top_p=config['generation']['top_p'],
+                    num_return_sequences=config['generation']['num_return_sequences_training'],
+                    pad_token_id=tokenizer.eos_token_id  # Set pad token ID explicitly
+                )
+
+            for i, sample_output in enumerate(sample_outputs):
+                logger.info("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+
+            model.train()
+
+        loss.backward()
+
+        optimizer.step()
+
+        scheduler.step()
+
+    return total_train_loss, t0
+
+
+def validation_batch(model: GPT2LMHeadModel, validation_dataloader: DataLoader, device: torchDevice):
+
+    logger.info("")
+    logger.info("Running Validation...")
+
+    t0 = time.time()
+
+    model.eval()
+
+    total_eval_loss = 0
+    # nb_eval_steps = 0
+
+    # Evaluate data for one epoch
+    for batch in validation_dataloader:
+
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[0].to(device)
+        b_masks = batch[1].to(device)
+
+        with no_grad():
+
+            outputs  = model(
+                b_input_ids,
+                # token_type_ids=None,
+                attention_mask = b_masks,
+                labels=b_labels
+            )
+
+            loss = outputs[0]
+
+        batch_loss = loss.item()
+        total_eval_loss += batch_loss
+
+    avg_val_loss = total_eval_loss / len(validation_dataloader)
+
+    return avg_val_loss, t0
   
 
 def train(
@@ -48,6 +177,8 @@ def train(
     patience = 0
     max_patience = config['training']['max_patience']
 
+    device = torch.device(device)
+
     model = model.to(device)
     best_model_state_dict = None
 
@@ -59,87 +190,23 @@ def train(
     attention_mask = inputs['attention_mask']  # The attention mask
 
     # Print the generated input tensor
-    logger.debug(input_ids)
-    logger.debug(attention_mask)
+    logger.debug(f"input_ids: {input_ids}")
+    logger.debug(f"attention_mask: {attention_mask}")
 
     for epoch_i in range(0, config['training']['epochs']):
 
-        # ========================================
-        #               Training
-        # ========================================
-
-        logger.info("")
-        logger.info('======== Epoch {:} / {:} ========'.format(epoch_i + 1, config['training']['epochs']))
-        logger.info('Training...')
-
-        t0 = time.time()
-
-        total_train_loss = 0
-
-        model.train()
-
-        for step, batch in enumerate(train_dataloader):
-
-            b_input_ids = batch[0].to(device)
-            b_labels = batch[0].to(device)
-            b_masks = batch[1].to(device)
-
-            model.zero_grad()
-
-            outputs = model(  
-                b_input_ids,
-                labels=b_labels,
-                attention_mask = b_masks,
-                token_type_ids=None
-            )
-
-            loss = outputs[0]
-
-            batch_loss = loss.item()
-            total_train_loss += batch_loss
-
-            # Get sample every x batches.
-            if step % config['training']['sample_every'] == 0 and not step == 0:
-
-                elapsed = format_time(time.time() - t0)
-                logger.info('  Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Total Elapsed this Epoch: {:}.'.format(step, len(train_dataloader), batch_loss, elapsed))
-
-                model.eval()
-
-                # sample_outputs = model.generate(
-                #     bos_token_id=random.randint(1,30000),
-                #     do_sample=True,
-                #     top_k=50,
-                #     max_length = 200,
-                #     top_p=0.95,
-                #     num_return_sequences=1
-                # )
-
-                with no_grad():
-                    sample_outputs = model.generate(
-                        # generated,
-                        input_ids=input_ids,
-                        attention_mask=attention_mask,  # Include the attention mask
-                        # bos_token_id=random.randint(1,30000),
-                        do_sample=config['generation']['do_sample'],
-                        temperature=config['generation']['temperature'],
-                        top_k=config['generation']['top_k'],
-                        max_length=config['generation']['max_length'],
-                        top_p=config['generation']['top_p'],
-                        num_return_sequences=config['generation']['num_return_sequences_training'],
-                        pad_token_id=tokenizer.eos_token_id  # Set pad token ID explicitly
-                    )
-
-                for i, sample_output in enumerate(sample_outputs):
-                    logger.info("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
-
-                model.train()
-
-            loss.backward()
-
-            optimizer.step()
-
-            scheduler.step()
+        total_train_loss, t0 = train_batch(
+            epoch_i=epoch_i, 
+            config=config, 
+            model=model, 
+            train_dataloader=train_dataloader, 
+            device=device,
+            input_ids=input_ids, 
+            attention_mask=attention_mask, 
+            tokenizer=tokenizer, 
+            optimizer=optimizer, 
+            scheduler=scheduler,
+        )
 
         # Calculate the average loss over all of the batches.
         avg_train_loss = total_train_loss / len(train_dataloader)
@@ -151,42 +218,7 @@ def train(
         logger.info("  Average training loss: {0:.2f}".format(avg_train_loss))
         logger.info("  Training epoch took: {:}".format(training_time))
 
-        # ========================================
-        #               Validation
-        # ========================================
-
-        logger.info("")
-        logger.info("Running Validation...")
-
-        t0 = time.time()
-
-        model.eval()
-
-        total_eval_loss = 0
-        # nb_eval_steps = 0
-
-        # Evaluate data for one epoch
-        for batch in validation_dataloader:
-
-            b_input_ids = batch[0].to(device)
-            b_labels = batch[0].to(device)
-            b_masks = batch[1].to(device)
-
-            with no_grad():
-
-                outputs  = model(
-                    b_input_ids,
-                    # token_type_ids=None,
-                    attention_mask = b_masks,
-                    labels=b_labels
-                )
-
-                loss = outputs[0]
-
-            batch_loss = loss.item()
-            total_eval_loss += batch_loss
-
-        avg_val_loss = total_eval_loss / len(validation_dataloader)
+        avg_val_loss, t0 = validation_batch(model=model, validation_dataloader=validation_dataloader, device=device)
 
         validation_time = format_time(time.time() - t0)
 
@@ -224,7 +256,26 @@ def train(
     return model, training_stats, total_t0
 
 
-def resume_training_if_possible(model, optimizer, scheduler, config):
+def resume_training_if_possible(
+        model: GPT2LMHeadModel, 
+        optimizer: AdamW, 
+        scheduler: LambdaLR, 
+        config: dict,
+    ) -> int:
+    """
+    Resume training if a checkpoint exists.
+
+    Args:
+    -----
+    model (GPT2LMHeadModel): the GPT2 model
+    optimizer (AdamW): the optimizer
+    scheduler (LambdaLR): the learning rate scheduler
+    config (dict): the configuration dictionary containing paths and other settings
+
+    Returns:
+    --------
+    int: the starting epoch for resuming training
+    """
     checkpoint_path = config['paths']['checkpoint_path']
     start_epoch = 0
     if os.path.exists(checkpoint_path):
@@ -236,6 +287,7 @@ def resume_training_if_possible(model, optimizer, scheduler, config):
         start_epoch = checkpoint['epoch'] + 1
         logger.info(f"Resuming from epoch {start_epoch}")
     return start_epoch
+
 
 # def train(model, device, train_dataloader, validation_dataloader, tokenizer,
 #           optimizer, scheduler, config, start_epoch=0, max_epochs=None):
